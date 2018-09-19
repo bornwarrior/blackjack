8992ac29b0b1b6904084d6ab40ca86b930a2467b
bornwarrior <j3patel@gmail.com>
2018-09-18 20:24:39 -0400

    Use of function

--- script.js
+++ script.js
@@ -4,19 +4,29 @@ let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
 let values = ['Ace','King','Queen','Jack',
       'Ten', 'Nine', 'Eight', 'Seven', 'Six',
       'Five','Four', 'Three', 'Two'];
-let deck = [];
 
-for(let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
-  for(let valueIdx = 0; valueIdx < values.length; valueIdx++) {
-    deck.push(values[valueIdx]+ ' of ' + suits[suitIdx]); 
+function createDeck() {
+  let deck = [];
+  for(let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
+    for(let valueIdx = 0; valueIdx < values.length; valueIdx++) {
+      deck.push(values[valueIdx]+ ' of ' + suits[suitIdx]); 
+    }
+    
+    return deck;
   }
+}
+
+let deck = createDeck();
+
+function getNextCard() {
+  return deck.shift();
 }
 
 for(let i = 0; i < deck.length;  i++ ) {
   console.log(deck[i]);
 }
 
-let playerCards = [deck[0], deck[2]];
+let playerCards = [ getNextCard() ,  getNextCard() ];
 console.log("Welcome to Blackjack!");
  
 console.log("You are dealt: ");
