fd0e01a9acde6a504cf5794a549aea931da6a806
bornwarrior <j3patel@gmail.com>
2018-09-19 06:33:08 -0400

    Created card object for the deck

--- script.js
+++ script.js
@@ -9,12 +9,18 @@ function createDeck() {
   let deck = [];
   for(let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
     for(let valueIdx = 0; valueIdx < values.length; valueIdx++) {
-      deck.push(values[valueIdx]+ ' of ' + suits[suitIdx]); 
+        let card = {
+          suit: suits[suitIdx],
+          value: values[valueIdx]
+        };
+         deck.push(card); 
+      }
+     
     }
     
     return deck;
   }
-}
+
 
 let deck = createDeck();
 
@@ -22,13 +28,17 @@ function getNextCard() {
   return deck.shift();
 }
 
-for(let i = 0; i < deck.length;  i++ ) {
-  console.log(deck[i]);
+function  getCardString(card) {
+  return card.value + ' of  ' + card.suit;
+  
 }
+//for(let i = 0; i < deck.length;  i++ ) {
+  //console.log(deck[i]);
+//}
 
 let playerCards = [ getNextCard() ,  getNextCard() ];
 console.log("Welcome to Blackjack!");
  
 console.log("You are dealt: ");
-console.log(" " + playerCards[0]);
-console.log(" " + playerCards[1]);
+console.log(" " + getCardString(playerCards[0]));
+console.log(" " + getCardString(playerCards[1]));
